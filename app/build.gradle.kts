387c92a3f4299e8181ec22e4659e5ea69d616915
Айдар Байков <aydar10.08@yandex.ru>
2025-03-20 22:16:34 +0500

    add dependences and fix App.java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
+    application
     id("com.github.ben-manes.versions") version "0.52.0"
     id("java")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
     mavenCentral()
