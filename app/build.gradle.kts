491c6ba90cd9266e3fbeba0088ede63592269d6d
Айдар Байков <aydar10.08@yandex.ru>
2025-03-20 21:36:35 +0500

    new gradle project, add .gitignore, add App.java

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
