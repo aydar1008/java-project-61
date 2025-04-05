5adc23c8e3efc7cdc2a983cba6a31a23eaa5fbce
Айдар Байков <aydar10.08@yandex.ru>
2025-04-05 22:30:42 +0500

    Fix "questions" in GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -11,8 +11,8 @@ public class GCD {
         String[] questions = new String[Engine.ROUNDS];
         String[] answers = new String[Engine.ROUNDS];
         for (int i = 0; i < questions.length; i++) {
-            int num1 = Engine.RANDOM.nextInt(MAX_RANDOM_NUMBER);
-            int num2 = Engine.RANDOM.nextInt(MAX_RANDOM_NUMBER);
+            int num1 = Engine.RANDOM.nextInt(1, MAX_RANDOM_NUMBER);
+            int num2 = Engine.RANDOM.nextInt(1, MAX_RANDOM_NUMBER);
             questions[i] = num1 + " " + num2;
             answers[i] = String.valueOf(num1 > num2 ? num1 / num2 : num2 / num1);
         }
