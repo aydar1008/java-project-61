f302fac7e11ed1982f074f7f627eab6363e3850f
Айдар Байков <aydar10.08@yandex.ru>
2025-04-06 23:49:41 +0500

    fix gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,9 +35,9 @@ tasks.test {
 // Конфигурация плагина org.sonarqube de5b130d40abfa74ea5ae85b5ba78525151dd357
 sonar {
     properties {
+        property "sonar.login", "de5b130d40abfa74ea5ae85b5ba78525151dd357"
         property("sonar.projectKey", "aydar1008_java-project-61")
         property("sonar.organization", "aydar1008")
-        property("sonar.login", System.getenv("SONAR_TOKEN"))
         property("sonar.java.binaries", "build/classes")
     }
 }
