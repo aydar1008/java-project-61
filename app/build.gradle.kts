b57902dec2f9c28453fcf838610b10ddc2e7afb3
Айдар Байков <aydar10.08@yandex.ru>
2025-04-08 21:42:09 +0500

    Fix sonar settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ tasks.test {
 // Конфигурация плагина org.sonarqube de5b130d40abfa74ea5ae85b5ba78525151dd357
 sonar {
     properties {
-        property("sonar.login", System.getenv("SONAR_TOKEN"))
+        property("sonar.token", System.getenv("SONAR_TOKEN"))
         property("sonar.projectKey", "aydar1008_java-project-61")
         property("sonar.organization", "aydar1008")
         property("sonar.java.binaries", "build/classes")
